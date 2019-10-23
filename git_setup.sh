04094bcaa3dc19c61d3bf0113d5cd030d8db6b6b
Troels Beck Krøgh <troels.kroegh@gmail.com>
2019-10-23 20:30:43 +0000

    Add git setup commands

---
+++ git_setup.sh
@@ -0,0 +1,5 @@
+git config --global user.name "Troels Beck Krøgh"
+git config --global user.email "troels.kroegh@gmail.com"
+# remember password: https://help.github.com/en/github/using-git/caching-your-github-password-in-git
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
