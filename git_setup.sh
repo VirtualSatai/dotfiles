385856b3fcb9c21de47079840660a95531c6a018
Troels Beck Krøgh <troels.kroegh@gmail.com>
2019-10-23 20:32:53 +0000

    Add git aliases

--- git_setup.sh
+++ git_setup.sh
@@ -3,3 +3,7 @@ git config --global user.email "troels.kroegh@gmail.com"
 # remember password: https://help.github.com/en/github/using-git/caching-your-github-password-in-git
 git config --global credential.helper cache
 git config --global credential.helper 'cache --timeout=3600'
+alias gac="git add . && git commit -m" # + commit message
+alias gp="git push" # + remote & branch names
+alias gl="git pull" # + remote & branch names
+alias gcb="git checkout -b" # + branch name
