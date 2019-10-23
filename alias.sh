f068f3292e1fba351f1fa49ee74069997133fe91
Troels Beck Krøgh <troels.kroegh@gmail.com>
2019-10-23 20:23:42 +0000

    Add common aliases

---
+++ alias.sh
@@ -0,0 +1,4 @@
+alias upd="apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y"
+alias etc="cd /etc"
+alias log="cd /nginx/logs/"
+alias nr="nginx -t && systemctl restart nginx"
